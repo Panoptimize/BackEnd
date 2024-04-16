cd8c72ac2ca00242c8106a5e7b5c8585383b3071
Ernesto Miranda <ernomitrovic@zohomail.com>
2024-04-16 17:32:57 -0600

    New repository created

---
+++ src/main/java/com/itesm/panoptimize/PanoptimizeApplication.java
@@ -0,0 +1,13 @@
+package com.itesm.panoptimize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanoptimizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanoptimizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itesm/panoptimize/PanoptimizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itesm.panoptimize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanoptimizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
