54645d3d64df12586783197724ce5072e78b11dd
lunarmelon <lunarmelon@tuta.io>
2024-04-16 17:59:04 -0600

    added dashboard endpoint (dto and controller)

---
+++ src/main/java/com/itesm/panoptimize/controller/DashboardController.java
@@ -0,0 +1,35 @@
+package com.tec.panoptimize.controller;
+
+import com.tec.panoptimize.dto.DashboardDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController {
+    @Operation(summary = "Get the dashboard data", description = "Get the dashboard data by time frame, agent and workspace number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Found the data",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = DashboardDTO.class))
+                    }),
+            @ApiResponse(responseCode = "404",
+                    description = "Data not found",
+                    content = @Content),
+    })
+    @PostMapping("/data")
+    public ResponseEntity<String> postData(@RequestBody DashboardDTO dashboardDTO) {
+        return new ResponseEntity<>("Data received", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/itesm/panoptimize/dto/DashboardDTO.java
@@ -0,0 +1,43 @@
+package com.tec.panoptimize.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+/*
+POST
+/dashboard/data
+{
+    timeframe: string,
+    agent: int []
+    workspace: int []
+}
+*/
+public class DashboardDTO {
+    @NotBlank
+    private String timeframe;
+    private int[] agents;
+    private int[] workspaces;
+
+    public String getTimeframe() {
+        return timeframe;
+    }
+
+    public void setTimeframe(String timeframe) {
+        this.timeframe = timeframe;
+    }
+
+    public int[] getAgents() {
+        return agents;
+    }
+
+    public void setAgents(int[] agents) {
+        this.agents = agents;
+    }
+
+    public int[] getWorkspaces() {
+        return workspaces;
+    }
+
+    public void setWorkspaces(int[] workspaces) {
+        this.workspaces = workspaces;
+    }
+}
