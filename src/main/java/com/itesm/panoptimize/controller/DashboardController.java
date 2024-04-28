dc7107357dcc83cae030dca46db09c5ff6175213
SebastianIL <a01661179@tec.mx>
2024-04-28 14:20:44 -0600

    KPI endpoint progress
    I updated the dependencies to be able to work with JSON object, I also added the service for Dashboard Data witch makes the requests to my local FastApi. On the DashboardDataController I added the metrics endpoint to simulate the endpoint the dashboard will call for all of its information

--- src/main/java/com/itesm/panoptimize/controller/DashboardController.java
+++ src/main/java/com/itesm/panoptimize/controller/DashboardController.java
@@ -10,10 +10,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -50,4 +47,5 @@ public class DashboardController {
             return ResponseEntity.notFound().build();
         }
     }
+
 }
