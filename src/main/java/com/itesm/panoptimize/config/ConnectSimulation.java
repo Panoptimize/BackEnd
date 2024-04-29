ff7dc816bd212cb46eb55d63cede70b6149683d7
Ernesto Miranda <ernomitrovic@zohomail.com>
2024-04-29 17:44:05 -0600

    models defined

--- src/main/java/com/itesm/panoptimize/config/ConnectSimulation.java
+++ src/main/java/com/itesm/panoptimize/config/ConnectSimulation.java
@@ -2,12 +2,12 @@ package com.itesm.panoptimize.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ConnectSimulation {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl(Constants.CONNECT_SIMULATION_URL).build();
     }
 }
---
+++ src/main/java/com/itesm/panoptimize/model/SatisfactionLevel.java
@@ -0,0 +1,2 @@
+package com.itesm.panoptimize.model;public class SatisfactionLevel {
+}
