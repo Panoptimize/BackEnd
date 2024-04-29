995951a7e6e795a92b738e05904ff6ff3c4ae33d
Carava Jojo <102180250+InakiVA@users.noreply.github.com>
2024-04-28 23:02:28 -0600

    actualización de cálculo de satisfacción

--- src/main/java/com/itesm/panoptimize/service/CalculateSatisfactionService.java
+++ src/main/java/com/itesm/panoptimize/service/CalculateSatisfactionService.java
@@ -9,18 +9,45 @@ import java.util.Map;
 @Service
 public class CalculateSatisfactionService {
 
+    public static int calcAnswerTimeQuality(int answer_time){
+        int quality=30;//we assign 30 max points for answer time and 70 to handle time
+        int leniency=20;//time we give the agent (in seconds) to answer without any penalty    
+        int rhythm=2;//how often (in seconds) a point is taken off of quality
+    
+        int penalty=(answer_time-leniency)/rhythm;//points to be taken off
+        quality-= penalty;
+        if (quality<0){
+          quality=0;
+        }
+        return quality;
+      }
+    
+    public static int calcHandleTimeQuality(int handle_time){
+        int quality=70;//we assign 30 max points for answer time and 70 to handle time
+        int leniency=90;//time we give the agent (in seconds) to handle without any penalty, maybe better use AHT
+        int rhythm=6;//how often (in seconds) a point is taken off of quality
+    
+        int penalty=(handle_time-leniency)/rhythm;//points to be taken off
+        quality-= penalty;
+        if (quality<0){
+          quality=0;
+        }
+        return quality;
+      }
+
     public String calculateSatisfaction(DashboardSatisfactionDTO satisfactionDTO){
         int speedOfAnswer = satisfactionDTO.getSpeedOfAnswer();
         int handleTime = satisfactionDTO.getHandleTime();
-        int afterCallworkTime = satisfactionDTO.getAfterCallworkTime();
         boolean abandoned = satisfactionDTO.getAbandoned();
 
         int satisfaction = 0;
 
-        if(speedOfAnswer < 30) { satisfaction++; }
-        if(handleTime < 300) { satisfaction++; }
-        if(afterCallworkTime < 300) { satisfaction++; }
-        if(!abandoned) { satisfaction++; }
+        if(abandoned) {satisfaction=0;}
+        else{            
+            satisfaction += calcAnswerTimeQuality(speedOfAnswer);
+            satisfaction += calcHandleTimeQuality(handleTime);
+            satisfaction /=20; //to switch case 0-4
+        }
 
         switch(satisfaction){
             case 0:
