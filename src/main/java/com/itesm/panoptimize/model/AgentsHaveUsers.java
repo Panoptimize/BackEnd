7a8a3a412626d36419d70e1ce55ed5be4a8f7b8a
Ernesto Miranda <ernomitrovic@zohomail.com>
2024-04-29 17:57:09 -0600

    All models declared

--- src/main/java/com/itesm/panoptimize/model/AgentsHaveUsers.java
+++ src/main/java/com/itesm/panoptimize/model/AgentsHaveUsers.java
@@ -1,2 +1,32 @@
-package com.itesm.panoptimize.model;public class AgentsHaveUsers {
+package com.itesm.panoptimize.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "agents_have_users")
+@IdClass(AgentsHaveUsersId.class)
+public class AgentsHaveUsers {
+
+    @Id
+    @Column(name = "supervisor_id", nullable = false)
+    private int supervisorId;
+
+    @Id
+    @Column(name = "agent_id", nullable = false)
+    private int agentId;
+
+    @ManyToOne
+    @JoinColumn(name = "supervisor_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+    private User supervisor;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+    private User agent;
+}
+
+class AgentsHaveUsersId implements Serializable {
+    private int supervisorId;
+    private int agentId;
 }
--- src/main/java/com/itesm/panoptimize/model/SatisfactionLevel.java
+++ src/main/java/com/itesm/panoptimize/model/SatisfactionLevel.java
@@ -1,2 +1,19 @@
-package com.itesm.panoptimize.model;public class SatisfactionLevel {
+package com.itesm.panoptimize.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "satisfaction_level")
+public class SatisfactionLevel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "satisfaction_level_id")
+    private int id;
+
+    @Column(name = "level_name", nullable = false)
+    private String levelName;
+
+    @ManyToOne
+    @JoinColumn(name = "call_call_id", nullable = false)
+    private Call call;
 }
