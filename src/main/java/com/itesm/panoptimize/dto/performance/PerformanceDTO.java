44b69a1429549e3a8d778eef4b000d6e7e9f3c8d
Antonio Rafael Cedillo Rodríguez <A01656823@tec.mx>
2024-04-29 07:19:49 -0600

    version 2 de performance endpoint
    falta probar el request (checarlo en Daily), compila. Falta integrar java docs.

--- src/main/java/com/itesm/panoptimize/dto/performance/PerformanceDTO.java
+++ src/main/java/com/itesm/panoptimize/dto/performance/PerformanceDTO.java
@@ -1,52 +1,21 @@
 package com.itesm.panoptimize.dto.performance;
 
-//Este endpoint depende de datos de otros endpoints para hacer la ponderacion:
-// - El endpoint de KPI de Nivel de servicio (SL) -> 30%
-// - El endpoint de KPI de Resolución a Primer Contacto (FCR) -> 30%
-// - El endpoint del KPI de Ocupación -> 40%
+//Este endpoint depende de datos de otros endpoints para hacer la ponderacion.
+//El to do esta en el servicio
+
+import java.util.List;
+
 public class PerformanceDTO {
-    private int fcr_KPI; //Extraido de otro endpoint, resolución primer contacto
-    private int ocup_KPI; //Extraido de otro endpoint, occupation
-    private int adhe_KPI; //Extraidoo de otro endpoint, adherence
 
-
-
-    public PerformanceDTO(int fcr_KPI, int ocup_KPI, int adhe_KPI) {
-        this.fcr_KPI = fcr_KPI;
-        this.ocup_KPI = ocup_KPI;
-        this.adhe_KPI = adhe_KPI;
+    public List<Double> getPerformanceData() {
+        return performanceData;
     }
 
-    public PerformanceDTO(){
-
+    public void setPerformanceData(List<Double> performanceData) {
+        this.performanceData = performanceData;
     }
 
-    public int getFcr_KPI() {
-        return fcr_KPI;
-    }
+    private List<Double> performanceData;
 
-    public void setFcr_KPI(int fcr_KPI) {
-        this.fcr_KPI = fcr_KPI;
-    }
-
-    public int getOcup_KPI() {
-        return ocup_KPI;
-    }
-
-    public void setOcup_KPI(int ocup_KPI) {
-        this.ocup_KPI = ocup_KPI;
-    }
-
-    public int getAdhe_KPI() {
-        return adhe_KPI;
-    }
-
-    public void setAdhe_KPI(int adhe_KPI) {
-        this.adhe_KPI = adhe_KPI;
-    }
-
-
-
-
-    }
+}
 
--- src/main/java/com/itesm/panoptimize/service/CalculatePerformance.java
+++ src/main/java/com/itesm/panoptimize/service/CalculatePerformance.java
@@ -1,4 +1,37 @@
 package com.itesm.panoptimize.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//TODO Creación de endpoints esenciales para performance.
+// - El endpoint de KPI de Nivel de servicio (SL) -> 30%
+// - El endpoint de KPI de Resolución a Primer Contacto (FCR) -> 40%
+// - El endpoint del KPI de Ocupación -> 30%
 public class CalculatePerformance {
+    public static List<Double> performanceCalculation(List<Double> serviceLevel, List<Double> firstContactResolution, List<Double> ocupation){
+
+        List<Double> performance = new ArrayList<>();
+        //Chequeo de longitud
+        int lenSl = serviceLevel.size();
+        int lenFCR = firstContactResolution.size();
+        int lenOC = ocupation.size();
+
+        //Suponiendo que las longitudes/tamaños son iguales
+        if ((lenSl == lenFCR) && (lenOC == lenFCR)){
+            System.out.println("The data samples are not of the same size.");
+            return performance;
+        } else{
+            for (int i=0;i<=lenSl;i++){
+                Double pData = 0.0;
+                double slPond = serviceLevel.get(i) * 0.3;
+                double fcrPond = firstContactResolution.get(i) * 0.4;
+                double  ocuPond = ocupation.get(i) * 0.3;
+                pData = slPond + fcrPond + ocuPond;
+                performance.add(pData);
+            }
+            return performance;
+
+        }
+
+    }
 }
