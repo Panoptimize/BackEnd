f160e466b772436e1cd20c0b30ef4fe6ad83a99e
Ernesto Miranda <ernomitrovic@zohomail.com>
2024-04-30 10:44:26 -0600

    Dashboard service with callKPI

--- src/main/java/com/itesm/panoptimize/service/DashboardService.java
+++ src/main/java/com/itesm/panoptimize/service/DashboardService.java
@@ -2,13 +2,19 @@ package com.itesm.panoptimize.service;
 
 import com.itesm.panoptimize.config.Constants;
 import com.itesm.panoptimize.dto.dashboard.DashboardDTO;
+import com.itesm.panoptimize.dto.metric.MetricRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
@@ -17,18 +23,24 @@ import java.util.List;
 @Service
 public class DashboardService {
 
-
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     @Autowired
-    public DashboardService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public DashboardService(WebClient webClient) {
+        this.webClient = webClient;
     }
 
-    private List<Double> callKPIs(DashboardDTO dashboardDTO) {
-        String url = "http://localhost:8080/dashboard/data/download";
-        ResponseEntity<List<Double>> responseEntity = restTemplate.exchange(url, HttpMethod.POST, null, new ParameterizedTypeReference<>() {
-        });
+    private double[] callKPIs(MetricRequest metricRequest) {
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.post();
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri("/get_metric_data");
+        WebClient.RequestHeadersSpec<?> headersSpec = bodySpec.bodyValue(metricRequest);
+        WebClient.ResponseSpec responseSpec = headersSpec
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .acceptCharset(StandardCharsets.UTF_8)
+                .retrieve();
+        Mono<ResponseEntity<double[]>> responseEntityMono = responseSpec.toEntity(double[].class);
+        ResponseEntity<double[]> responseEntity = responseEntityMono.block();
+        assert responseEntity != null;
         return responseEntity.getBody();
     }
 
