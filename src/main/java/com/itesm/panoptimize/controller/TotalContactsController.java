7bc3409e56346c92cc0f3cef5690fbe6c3d385b2
JoseRobertoGC <a01656294@tec.mx>
2024-04-30 20:27:44 -0600

    Dummy data generation and javaDOC documentation

--- src/main/java/com/itesm/panoptimize/controller/TotalContactsController.java
+++ src/main/java/com/itesm/panoptimize/controller/TotalContactsController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+/**
+ * Rest controller for managing total contacts.
+ * Handles HTTP requests for operations related to total contacts statistics.
+ */
 @RestController
 @RequestMapping("/api/total-calls")
 public class TotalContactsController {
@@ -14,6 +18,10 @@ public class TotalContactsController {
         this.totalContactsService = totalContactsService;
     }
 
+    /**
+     * Endpoint to retrieve the monthly activity of contacts.
+     * @return a list of integers representing the number of contacts per month
+     */
     @GetMapping("/monthly-activity")
     public List<Integer> get() {
         return totalContactsService.countMonthlyContacts();
