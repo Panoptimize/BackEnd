50aef0c64c55a55faa6f69f42c73c19e99a97732
CesarCeballosC <105232610+CesarCeballosC@users.noreply.github.com>
2024-05-01 17:29:41 -0600

    Update DashboardController.java

--- src/main/java/com/itesm/panoptimize/controller/DashboardController.java
+++ src/main/java/com/itesm/panoptimize/controller/DashboardController.java
@@ -10,12 +10,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
