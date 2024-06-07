20c8071b5bcdddc3b0e328df2ab91daed6c93636
owenrocs <102175853+owenrocs@users.noreply.github.com>
2024-06-06 19:57:37 -0600

    Action Center changes

--- src/main/java/com/itesm/panoptimize/controller/ContactController.java
+++ src/main/java/com/itesm/panoptimize/controller/ContactController.java
@@ -2,10 +2,15 @@ package com.itesm.panoptimize.controller;
 
 import com.itesm.panoptimize.dto.contact.ContactDTO;
 import com.itesm.panoptimize.dto.contact.CreateContactDTO;
+import com.itesm.panoptimize.dto.contact.SearchContactsDTO;
+import com.itesm.panoptimize.dto.contact.SearchContactsResponseDTO;
+import com.itesm.panoptimize.service.ContactSearchService;
 import com.itesm.panoptimize.service.ContactService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +20,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/contact")
 public class ContactController {
     private final ContactService contactService;
+    private final ContactSearchService contactSearchService;
 
-    public ContactController(ContactService contactService) {
+    public ContactController(ContactService contactService, ContactSearchService contactSearchService) {
         this.contactService = contactService;
+        this.contactSearchService = contactSearchService;
     }
 
     @Operation(summary = "Get contact by id")
@@ -65,5 +72,11 @@ public class ContactController {
     @GetMapping("/")
     public ResponseEntity<Page<ContactDTO>> getAllContacts(Pageable pageable) {
         return ResponseEntity.ok(contactService.getAllContacts(pageable));
+    }
+
+    @PostMapping(value = "/search", produces = "application/json")
+    public ResponseEntity<SearchContactsResponseDTO> searchContacts(@Valid @RequestBody SearchContactsDTO searchContactsDTO) {
+        SearchContactsResponseDTO response = contactSearchService.searchContacts(searchContactsDTO);
+        return ResponseEntity.ok(response);
     }
 }
